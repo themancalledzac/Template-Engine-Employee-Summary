eb7df4f5f756a509fe72b2fde028b349a52de476
Zac Edens <themancalledzac@gmail.com>
2020-12-09 11:37:31 -0800

    Employee.js now tests properly.

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -1,44 +1,83 @@
 const Employee = require("../lib/Employee");
 
+// is employee.js an object? 
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
   expect(typeof(e)).toBe("object");
 });
 
 test("Can set name via constructor arguments", () => {
+  // Arrange
   const name = "Alice";
+
+  // Act (calling the constructor)
   const e = new Employee(name);
+
+  // Assert (and expext our name property is equal to the name passed in)
   expect(e.name).toBe(name);
 });
 
 test("Can set id via constructor argument", () => {
+
+  // Arrange (faking ID, arbitrary value)
   const testValue = 100;
+
+  // Act (test name added, create object)
   const e = new Employee("Foo", testValue);
+
+  // Assert (that it has that expected value)
   expect(e.id).toBe(testValue);
 });
 
 test("Can set email via constructor argument", () => {
+
+  // Arrange (test value for the email)
   const testValue = "test@test.com";
+
+  // Act (create an employee object that includes email)
   const e = new Employee("Foo", 1, testValue);
+
+  // Assert (object now has email property=testemail)
   expect(e.email).toBe(testValue);
 });
 
 test("Can get name via getName()", () => {
+
+  // Arrange (test value)
   const testValue = "Alice";
   const e = new Employee(testValue);
-  expect(e.getName()).toBe(testValue);
+
+  // Act (calling gitname action)
+  const result = e.getName();
+
+  // Assert (test value correct)
+  expect(result).toBe(testValue);
 });
 
 test("Can get id via getId()", () => {
+
+  // Arrange
   const testValue = 100;
   const e = new Employee("Foo", testValue);
-  expect(e.getId()).toBe(testValue);
+
+  // Act
+  const result = e.getID();
+
+  // Assert
+  expect(result).toBe(testValue);
 });
 
 test("Can get email via getEmail()", () => {
+
+  // Arrange
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
-  expect(e.getEmail()).toBe(testValue);
+
+  // Act
+  const result = e.getEmail();
+
+  // Assert
+  expect(result).toBe(testValue);
 });
 
 test("getRole() should return \"Employee\"", () => {
